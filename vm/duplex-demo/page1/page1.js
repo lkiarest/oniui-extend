ea4c51524774b2788af1e6bafb49f82a3c06ee83
qintx <rushi_wowen@163.com>
2015-09-01 22:39:25 +0800

    duplex性能测试

---
+++ vm/duplex-demo/page1/page1.js
@@ -0,0 +1,29 @@
+define([
+    ], function() {
+
+    var vm = avalon.define({
+        $id: "page1",
+        aaa: "aaa",
+        bbb: "bbb",
+        ccc: "ccc",
+        ddd: "ddd"
+    });
+
+    return avalon.controller(function($ctrl) {
+        // 视图渲染后，意思是avalon.scan完成
+        $ctrl.$onRendered = function() {
+
+        };
+        // 进入视图
+        $ctrl.$onEnter = function() {
+        };
+        // 对应的视图销毁前
+        $ctrl.$onBeforeUnload = function() {
+            console.log("before unload page1, avalon.$$subscribers: " + avalon.$$subscribers.length);
+            console.log(avalon.$$subscribers);
+        };
+        // 指定一个avalon.scan视图的vmodels，vmodels = $ctrl.$vmodels.concact(DOM树上下文vmodels)
+        $ctrl.$vmodels = [];
+    });
+
+});
