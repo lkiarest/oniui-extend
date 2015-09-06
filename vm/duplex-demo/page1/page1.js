64467ed05b051536aad237791eb83e12405c3579
qintx <rushi_wowen@163.com>
2015-09-06 23:25:11 +0800

    optimize memory leak

--- vm/duplex-demo/page1/page1.js
+++ vm/duplex-demo/page1/page1.js
@@ -3,24 +3,33 @@ define([
 
     var vm = avalon.define({
         $id: "page1",
-        aaa: "aaa",
-        bbb: "bbb",
-        ccc: "ccc",
-        ddd: "ddd",
+        o: {},
+        ccc: "",
+        ddd: "",
         selectedRoom: "",
-        roomList: [{name: "room1", value:"0"}, {name: "room2", value:"1"}, {name: "room3", value:"2"}]
+        roomList: []
     });
 
     return avalon.controller(function($ctrl) {
         // 视图渲染后，意思是avalon.scan完成
         $ctrl.$onRendered = function() {
-
+            
         };
         // 进入视图
         $ctrl.$onEnter = function() {
+            vm.roomList = [{name: "room1", value:"0"}, {name: "room2", value:"1"}, {name: "room3", value:"2"}];
+            vm.o = {
+                aaa: "aaa",
+                bbb: "bbb"
+            };
+            vm.ccc = "ccc";
+            vm.ddd = "ddd";
+            vm.selectedRoom = "";
         };
         // 对应的视图销毁前
         $ctrl.$onBeforeUnload = function() {
+            vm.roomList.clear();
+            vm.o = {};
             console.log("before unload page1, avalon.$$subscribers: " + avalon.$$subscribers.length);
             console.log(avalon.$$subscribers);
         };
