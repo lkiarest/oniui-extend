22360eda18325388a6f7a4a6d4b80c9f19c5d648
lkiarest <rushi_wowen@163.com>
2015-10-17 09:39:47 +0800

    超出指定单元格宽度自动省略，设置为单行显示

--- hygrid/hygrid.js
+++ hygrid/hygrid.js
@@ -12,17 +12,28 @@ define(["avalon",
     "css!./hygrid.css"
 ], function(avalon, template) {
 
+    var isFunction = typeof alert === "object" ? function (fn) {
+        try {
+            return /^\s*\bfunction\b/.test(fn + "");
+        } catch (e) {
+            return false;
+        }
+    } : function (fn) {
+        return Object.prototype.toString.call(fn) === "[object Function]";
+    };
+
     var widget = avalon.ui.hygrid = function (element, data, vmodels) {
 
         var vmodel = avalon.define(data.hygridId, function(vm) {
 
             var $element = avalon(element),
                 options = data.hygridOptions,
-                _columns = options.columns;
-                // _rawdata = options.data;
+                _columns = options.columns,
+                _rawdata = options.data;
 
             var defaultPager = avalon.mix(
                 {
+                    inputPage: 1,
                     perPages: 10,
                     currentPage: 1,
                     totalPages: 1,
@@ -32,13 +43,13 @@ define(["avalon",
             avalon.mix(vm, options);
             vm.columns = _columns;
             vm.pager = defaultPager;
-            // vm.rawData = _rawdata;
+            // vm.rawData = options.data;
 
             vm.widgetElement = element;
             vm.rootElement = element;
             vm.$skipArray = ["widgetElement", "rootElement"]
 
-            vm.data = createRowData(_columns, options.data, options.htmlHelper, data.hygridId);
+            vm.data = createRowData(_columns, _rawdata, options.htmlHelper, data.hygridId);
 
             if (vm.pageable) {
                 vm.currentPage = vm.pager.currentPage;
@@ -55,30 +66,45 @@ define(["avalon",
             };
 
             vm.$remove = function() {
+                _rawdata = [];
                 vmodel.data.clear();
                 vmodel.data = null;
                 vmodel.columns = null;
                 vmodel.pager = null;
                 vmodel.htmlHelper = null;
+                vmodel.checkedRows = null;
                 vmodel.widgetElement.innerHTML = "";
                 vmodel.widgetElement = null;
                 vm.rootElement = null;
+                vmodel.onExportClicked = avalon.noop;
             };
 
             vm.render = function(newData) {
                 // vmodel.rawData.clear();
                 vmodel.data.clear();
+                _rawdata = [];
                 if (newData && newData.length > 0) {
                     // vmodel.rawData = newData;
+                    _rawdata = newData;
                     vmodel.data = createRowData(_columns, newData, vmodel.htmlHelper, data.hygridId);
                 }
             };
 
             vm.setCurrentPage = function(current) {
-                vmodel.currentPage = vmodel.pager.currentPage = current ? current : 0;
+                vmodel.currentPage = vmodel.pager.inputPage = (vmodel.pager.currentPage = current ? current : 0);
+            };
+
+            vm.pageScope = function() {
+                var curPage = vmodel.currentPage;
+                var perPages = vmodel.pager.perPages;
+                var total = vmodel.pager.totalItems;
+                var start = (curPage - 1) * perPages + 1;
+                var end = Math.min(curPage*perPages, total);
+                return " 第 " + start + " - " + end + " 条";
             };
 
             vm.setTotal = function(total) {
+                vmodel.pager.totalItems = total;
                 vmodel.pager.totalPages = Math.ceil(total / vmodel.pager.perPages);
             };
 
@@ -105,7 +131,7 @@ define(["avalon",
             };
 
             vm.jumpTo = function() {
-                vmodel.jump(vmodel.currentPage);
+                vmodel.jump(vmodel.pager.inputPage);
             };
 
             vm.jump = function(toPage) {
@@ -119,6 +145,7 @@ define(["avalon",
                 }
 
                 vmodel.setCurrentPage(toPage);
+                // vmodel.pager.inputPage = toPage;
 
                 if (isFunction(options.pager.onJump)) {
                     options.pager.onJump(toPage);
@@ -144,7 +171,7 @@ define(["avalon",
 
             vm.getSelected = function() {
                 var datas = [];
-                var modelData = vmodel.data.$model;
+                var modelData = _rawdata;
                 vmodel.data.every(function(el, i) {
                     if (el.selected) {
                         datas.push(modelData[i]);
@@ -178,15 +205,16 @@ define(["avalon",
                 var key = citem.key;
                 var formater = citem.format;
                 var align = citem.align ? citem.align : "center";
+                var width = citem.width;
 
                 if (formater && htmlHelper[formater]) {
                     var formated = htmlHelper[formater](widgetId, key, j, rowItem[key], rowItem);
-                    row.data.push({"value": formated, "align": align});
+                    row.data.push({"value": formated, "align": align, width: width});
                 } else {
                     if (rowItem[key] !== undefined) {
-                        row.data.push({"value": rowItem[key], "align": align});
+                        row.data.push({"value": rowItem[key], "align": align, width: width});
                     } else {
-                        row.data.push({"value": "", "align": "center"});
+                        row.data.push({"value": "", "align": "center", width: width});
                     }
                 }
             }
@@ -200,12 +228,14 @@ define(["avalon",
     widget.defaults = {
         selectable: false,
         allchecked: false,
+        showExport: false,
         pageable: true,
         checkedRows: [],
         currentPage: 1,
         htmlHelper: {},
         pager: {},
-        data: []
+        data: [],
+        onExportClicked: avalon.noop
     };
 
     return avalon;
